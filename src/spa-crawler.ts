5d2a528387c7789fb2dae27706c58a296ba2d668
naisho <vertigodragon@gmail.com>
2018-05-31 19:08:46 -0400

    optimize to use one browser instance

--- src/spa-crawler.ts
+++ src/spa-crawler.ts
@@ -4,7 +4,6 @@ import { Config } from './config/config';
 import { Offer } from './config/Offer';
 import { Object } from 'aws-sdk/clients/s3';
 
-
 /**
  * Uses puppeteer to scrape page return an object
  * with specified parameters.  Function takes a config
@@ -14,15 +13,25 @@ import { Object } from 'aws-sdk/clients/s3';
  */
 export async function scrapePrice(config: Config): Promise<Offer[]> {
     const options: {[option: string]: puppeteer.LaunchOptions} = {
-        default: {},
+        default: {
+            executablePath: `./node_modules/puppeteer/.local-chromium/linux-549031/chrome-linux/chrome`,
+        },
         debug: {
             headless: false,
             slowMo: 100,
         },
     };
 
-    console.debug(`Starting browser...`);
-    const browser = await puppeteer.launch(options.default);
+    let browser: puppeteer.Browser;
+    let isInstance: boolean = false;
+    if (config.browser !== undefined) {
+        console.log(`Existing browser instance found.`)
+        isInstance = true;
+        browser = config.browser;
+    } else {
+        console.log(`Starting browser...`);
+        browser = await puppeteer.launch(options.default);
+    }
     const page = await browser.newPage();
     await page.setRequestInterception(true);
     page.on("request", (request) => {
@@ -33,23 +42,23 @@ export async function scrapePrice(config: Config): Promise<Offer[]> {
         }
     });
     await page.goto(config.url, {
-        waitUntil: "networkidle0",
+        waitUntil: "domcontentloaded",
     });
     await page.waitForSelector(config.onLoad);
-    console.debug(`Page loaded.`);
+    console.log(`Page loaded.`);
     let html = await page.content();
 
     const products = [];
 
     let $ = cheerio.load(html);
     let nodes = $(config.container);
-    console.debug(`Found ${nodes.length} nodes.`);
+    console.log(`Found ${nodes.length} nodes.`);
 
     if ((config.pagination) && (config.pagination.type === "infinite")) {
-        console.debug(`Resolving infinite scroll...`);
+        console.log(`Resolving infinite scroll...`);
         let prevNodes: Cheerio;
         do {
-            console.debug(`Now have ${nodes.length} nodes.`);
+            console.log(`Now have ${nodes.length} nodes.`);
             prevNodes = nodes;
             await config.pagination.action(page);
             try {
@@ -59,7 +68,7 @@ export async function scrapePrice(config: Config): Promise<Offer[]> {
                     }
                 );
             } catch (error) {
-                console.debug(`Finished scrolling (probably).`);
+                console.log(`Finished scrolling (probably).`);
             }
             await page.waitForSelector(
                 config.pagination.loader,
@@ -69,27 +78,41 @@ export async function scrapePrice(config: Config): Promise<Offer[]> {
             $ = cheerio.load(html);
             nodes = $(config.container);
         } while (nodes.length > prevNodes.length);
-        console.debug(`Finished scrolling.`);
+        console.log(`Finished scrolling.`);
     }
 
-    for (let i = 0; i < nodes.length; i++) {
-        console.debug(`Parsing node ${i+1} of ${nodes.length}`);
-        const node = nodes[i];
-        const product: {[field: string]: string} = {};
-        for (const field in config.fields) {
-            if ({}.hasOwnProperty.call(config.fields, field)) {
-                // @ts-ignore: scrapePrice 'Index signature is missing'
-                const value = await config.fields[field](node, $, scrapePrice);
-                if (value !== "") {
-                    product[field] = value;
-                    // console.log(field, product[field]);
+    if (nodes.length > 0) {
+        const batchSize = 10;
+        for (let i = 0; i < nodes.length; i++) {
+            console.log(`Parsing node ${i+1} of ${nodes.length}`);
+            const node = nodes[i];
+            const product: {[field: string]: string} = {};
+            for (const field in config.fields) {
+                if ({}.hasOwnProperty.call(config.fields, field)) {
+                    // @ts-ignore: scrapePrice 'Index signature is missing'
+                    let value = config.fields[field](node, $, scrapePrice, browser);
+                    // if (value !== "") {
+                        product[field] = value;
+                    //     console.log(field, product[field]);
+                    // }
                 }
             }
+            products.push(product);
+            if ((i % batchSize) == 0) {
+                console.log(`Waiting for all products to resolve...`)
+                await Promise.all(products.map(product => product.data));
+            }
         }
-        products.push(product);
     }
 
-    console.debug(`Got all products:`, products);
-    browser.close();
+    console.log(`Got all products:`, products);
+    await Promise.all(products.map(product => product.data));
+    if (isInstance) {
+        console.log(`Closing page...`)
+        page.close();
+    } else {
+        console.log(`Closing browser...`)
+        browser.close();
+    }
     return products;
 }
